09ed0766d37fe5aaa900288128192873d7664d30
Prathap1003 <prathapforeducational@gmail.com>
2026-08-03 15:10:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (75.32%) - LeetHub

--- LeetCode/Medium/0877-stone-game/0877-stone-game.py
+++ LeetCode/Medium/0877-stone-game/0877-stone-game.py
@@ -1,21 +1,23 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        alice=0
-        bob=0
-        nums=piles.copy()
-        while len(piles)>0:
-            alice+=piles[0]
-            del piles[0]
-            bob+=piles[0]
-            del piles[0]
-        chance1=0
-        chance2=0
-        s=len(nums)//2
-        while len(nums)>0:
-            chance1+=nums[-1]
-            del nums[-1]
-            chance2+=nums[-1]
-            del nums[-1]
-        return alice>bob or chance1>chance2
+        # alice=0
+        # bob=0
+        # nums=piles.copy()
+        
+        # while len(piles)>0:
+        #     alice+=piles[0]
+        #     del piles[0]
+        #     bob+=piles[0]
+        #     del piles[0]
+
+        # chance1=0
+        # chance2=0
+        # while len(nums)>0:
+        #     chance1+=nums[-1]
+        #     del nums[-1]
+        #     chance2+=nums[-1]
+        #     del nums[-1]
+        # return alice>bob or chance1>chance2
+        return True
 
         
