4fe6f007d8a2035859eccd1fff23c0d9e5143239
Prathap1003 <prathapforeducational@gmail.com>
2026-08-13 16:33:05 +0530

    Time: 334 ms (5%), Space: 64 MB (5.42%) - LeetHub

---
+++ LeetCode/Easy/2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diagonalPrime(self, nums: List[List[int]]) -> int:
+        s=(max(max(row) for row in nums))
+        box=[1]*(s+1)
+        box[0],box[1]=0,0
+        for i in range(2,int(s**0.5)+1):
+            if box[i]:
+                for j in range(i*i,s+1,i):
+                    box[j]=0
+        maximum=0
+        for i in range(len(nums[0])):
+            if box[nums[i][i]]:
+                maximum=max(maximum,nums[i][i])
+        for i in range(len(nums[0])):
+            if box[nums[i][len(nums[0])-i-1]]:
+                maximum=max(maximum,nums[i][len(nums[0])-i-1])
+        return maximum
+
+        
