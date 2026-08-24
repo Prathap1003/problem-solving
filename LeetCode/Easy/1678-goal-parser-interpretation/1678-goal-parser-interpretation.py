f813a6bb78f65965654cdd4d798d0cc24f39dd76
Prathap1003 <prathapforeducational@gmail.com>
2026-08-24 22:56:28 +0530

    Time: 51 ms (17.16%), Space: 19.3 MB (45.49%) - LeetHub

--- LeetCode/Easy/1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ LeetCode/Easy/1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,8 +1,6 @@
 class Solution:
     def interpret(self, command: str) -> str:
         string=""
-        if len(command)==1:
-            return "G"
         for i in range(len(command)-1):
             if command[i]=="G":
                 string+="G"
