856977f060ff2eed0baf3669eaa58122b2619865
Prathap1003 <prathapforeducational@gmail.com>
2026-08-24 18:47:33 +0530

    Time: 1485 ms (5.35%), Space: 19.2 MB (85.06%) - LeetHub

--- LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -3,17 +3,8 @@ class Solution:
         count=0
         answer=0
         return_value=0
-        for i in range(2**len(nums)):
-            j=0
-            answer=0
-            while i>0:
-                if i&1>0:
-                    answer|=nums[j]
-                else:
-                    pass
-                j+=1
-                i=i>>1
-            count=max(count,answer)
+        for i in nums:
+            count|=i
         for i in range(2**len(nums)):
             j=0
             val=0
