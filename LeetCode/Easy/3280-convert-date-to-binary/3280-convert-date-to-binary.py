642b0dcabb9117fe680aed953f5a7d6185947477
Prathap1003 <prathapforeducational@gmail.com>
2026-08-24 16:23:40 +0530

    Time: 1 ms (27.98%), Space: 19.2 MB (83.06%) - LeetHub

--- LeetCode/Easy/3280-convert-date-to-binary/3280-convert-date-to-binary.py
+++ LeetCode/Easy/3280-convert-date-to-binary/3280-convert-date-to-binary.py
@@ -2,6 +2,7 @@ class Solution:
     def convertDateToBinary(self, date: str) -> str:
         string=""
         res=""
+        date+='.'
         for i in range(len(date)):
             if ord(date[i])>=48 and ord(date[i])<=57:
                 res+=date[i]
@@ -10,8 +11,7 @@ class Solution:
                 string+=format(ans,'0b')
                 string+='-'
                 res=""
-        string+=format(int(date[len(date)-2:]),'0b')
-        return string
+        return string[:len(string)-1]
 
 
         
