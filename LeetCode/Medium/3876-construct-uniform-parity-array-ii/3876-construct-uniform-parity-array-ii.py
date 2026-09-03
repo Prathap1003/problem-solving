4c99cb4464cd4021c7d2e5e35d85d5f2cebe53e8
Prathap1003 <prathapforeducational@gmail.com>
2026-09-03 22:59:50 +0530

    Time: 57 ms (62.8%), Space: 35.1 MB (85.98%) - LeetHub

--- LeetCode/Medium/3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ LeetCode/Medium/3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        evens=[]
-        odds=[]
+        min_even=1000000000
+        min_odd=1000000000
         for i in nums1:
             if i&1==0:
-                evens.append(i)
+                min_even=min(min_even,i)
             else:
-                odds.append(i)
-        if not odds or not evens:
+                min_odd=min(min_odd,i)
+        if min_odd==1000000000 or min_even==1000000000:
             return True
-        if min(odds)<min(evens):
+        if min_odd<min_even:
             return True
         else:
             return False
