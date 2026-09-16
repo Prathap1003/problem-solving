7190ad0c435f7d497cb609a90bf5b9f99f0f604c
Prathap1003 <prathapforeducational@gmail.com>
2026-09-16 08:04:03 +0530

    Time: 211 ms (75.89%), Space: 24.2 MB (11.6%) - LeetHub

--- LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,36 +1,33 @@
 class Solution:
 
-    def possible(self,days,w,cap):
+    def solve(self,w,mid,days):
         
         day = 1
         ans = 0
         for i in w:
-            if(i > cap): return False
-            if(ans + i <= cap):
-                ans = ans + i
-            else:
-                day =  day + 1
-                ans = i
-        return day <= days
-
+            if i>mid:
+                return False
+            ans+=i
+            if ans>mid:
+                ans=i
+                day+=1
+        print(day)
+        return days>=day
     def shipWithinDays(self, w: List[int], days: int) -> int:
         #1 2 2 3 4 4
         #
        # w.sort()
-        low = 0
-        high = 1000000000
-        ans = 1000000000
-        while(low <= high):
-            mid = (low + high) >> 1
-
-            if(self.possible(days,w,mid)):
-                ans = min(ans,mid)
-                high = mid - 1
+        l= min(w)
+        r =sum(w)
+        val =0
+        while(l <= r):
+            mid=(l+r)//2
+            print(l,r,mid)
+            if self.solve(w,mid,days):
+                val=mid
+                r=mid-1
             else:
-                low = mid + 1
-        return ans
-
-            
-
+                l=mid+1
+        return val
         
         
