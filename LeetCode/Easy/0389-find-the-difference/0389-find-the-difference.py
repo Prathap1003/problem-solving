f67c4d150bf3d195eeeb045ea420e2d0237d2506
Boya Prathap <prathapforeducational@gmail.com>
2026-09-22 11:00:21 +0530

    Time: 0 ms (100%), Space: 19.1 MB (91.25%) - LeetHub

---
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res = 0
+        for i in range(len(s)):
+            res^=ord(s[i])^ord(t[i])
+        res^=ord(t[-1])
+        return chr(res)
+        
